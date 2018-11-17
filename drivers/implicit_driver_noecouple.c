6c674684fa284bd4c93cfbd9e689d0742123bda0
Jeff Haack <haack@lanl.gov>
2018-11-16 17:03:15 -0700

    Clanged

--- drivers/implicit_driver_noecouple.c
+++ drivers/implicit_driver_noecouple.c
@@ -1,30 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "../src/implicit.h"
 #include <gsl/gsl_matrix.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
   int nspec = 2;
   double dt = 1.0e-9;
-  int i,j, dim;
+  int i, j, dim;
 
   int conserveFlag = 0;
 
   double **nu = malloc(nspec * sizeof(double *));
-  for(i=0;i<nspec; i++) {
+  for (i = 0; i < nspec; i++) {
     nu[i] = malloc(nspec * sizeof(double));
   }
 
-  double **v     = malloc(nspec * sizeof(double *));
-  double **vnew  = malloc(nspec * sizeof(double *));
+  double **v = malloc(nspec * sizeof(double *));
+  double **vnew = malloc(nspec * sizeof(double *));
   double ***vmix = malloc(nspec * sizeof(double **));
-  for(i=0;i< nspec;i++) {
-    v[i]     = malloc(3*sizeof(double));
-    vnew[i]  = malloc(3*sizeof(double));
-    vmix[i]  = malloc(nspec * sizeof(double *));
-    for(j=0;j<nspec;j++)
-      vmix[i][j] = malloc(3*sizeof(double));
+  for (i = 0; i < nspec; i++) {
+    v[i] = malloc(3 * sizeof(double));
+    vnew[i] = malloc(3 * sizeof(double));
+    vmix[i] = malloc(nspec * sizeof(double *));
+    for (j = 0; j < nspec; j++)
+      vmix[i][j] = malloc(3 * sizeof(double));
   }
 
   double m[nspec];
@@ -33,27 +33,26 @@ int main() {
   double T[nspec];
   double Tnew[nspec];
   double **Tmix = malloc(nspec * sizeof(double *));
-  for(i=0;i<nspec;i++)
-    Tmix[i] = malloc(nspec*sizeof(double));
-  
+  for (i = 0; i < nspec; i++)
+    Tmix[i] = malloc(nspec * sizeof(double));
 
-  //Put in some dummy values
-  for(i=0;i<nspec;i++) {
+  // Put in some dummy values
+  for (i = 0; i < nspec; i++) {
     m[i] = 1.0;
     n[i] = 2.0;
 
-    v[i][0] =  0.0;
-    v[i][1] =  0.0;
-    v[i][2] =  0.0;
+    v[i][0] = 0.0;
+    v[i][1] = 0.0;
+    v[i][2] = 0.0;
 
     T[i] = 1.0 + i;
 
-    for(j=0;j<nspec;j++)
+    for (j = 0; j < nspec; j++)
       nu[i][j] = 1.0e10;
   }
 
   double amu = 1.6605e-24;
-  m[0] = 12*amu;
+  m[0] = 12 * amu;
   m[1] = amu;
 
   n[0] = 1.0e23;
@@ -65,7 +64,8 @@ int main() {
   T[0] = 200.0;
   T[1] = 100.0;
 
-  implicitGetVelocitiesTemperaturesLinear(n, v, T, nu, m, dt, nspec, conserveFlag, vnew, vmix, Tnew, Tmix);
+  implicitGetVelocitiesTemperaturesLinear(n, v, T, nu, m, dt, nspec,
+                                          conserveFlag, vnew, vmix, Tnew, Tmix);
 
   printf("New velocity\n");
   for (i = 0; i < nspec; i++)
@@ -100,7 +100,8 @@ int main() {
   /*
   printf("\n\n\n\n nonlinear solve\n\n\n------------------\n");
 
-  implicitGetVelocitiesTemperaturesNonlinear(n, v, T, m, dt, nspec, conserveFlag, vnew, vmix, Tnew, Tmix);
+  implicitGetVelocitiesTemperaturesNonlinear(n, v, T, m, dt, nspec,
+  conserveFlag, vnew, vmix, Tnew, Tmix);
 
   printf("New velocity\n");
   printf("----------------\n");
