6b60b76f7e2f4a39ab2c0f666b7f26bf91c9bb44
Jeff Haack <haack@lanl.gov>
2019-01-11 16:18:57 -0700

    Adding TNB files to be integrated into main code

---
+++ src/TNB.h
@@ -0,0 +1,13 @@
+void initializeTNB(int Nv_in, double **c_in, double **wts_in);
+
+double GetTNB_dt(double mu, double *in, double *c1, int sp, int sp2);
+double GetReactivity_dt(double mu, double *in, double *in2, int sp, int sp2);
+
+double GetTNB_dd_He(double mu, double *in, double *c1, int sp, int sp2);
+double GetReactivity_dd_He(double mu, double *in, double *in2, int sp, int sp2);
+
+double GetTNB_dd_T(double mu, double *in, double *c1, int sp, int sp2);
+double GetReactivity_dd_T(double mu, double *in, double *in2, int sp, int sp2);
+
+double GetTNB_tt(double mu, double *in, double *c1, int sp, int sp2);
+double GetReactivity_tt(double mu, double *in, double *in2, int sp, int sp2);
